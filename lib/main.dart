31bfa32928c912f860a644567042d3bdfea686c5
Ehnamuram Enoch <enirox001@gmail.com>
2022-04-05 20:41:57 +0100

    completed main calculator page

--- lib/main.dart
+++ lib/main.dart
@@ -23,104 +23,397 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+var weight = 56;
+var height = 162;
+var age = 29;
+List<String> weightCategory = [
+  'kg',
+  'lbs',
+];
+List<String> heightCategory = [
+  'cm',
+  'in',
+];
+String weightDropdownValue = 'kg';
+String heightDropdownValue = 'cm';
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: SafeArea(
-        child: Scaffold(
-          backgroundColor: const Color(0xFF000000),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Icon(
-                      Icons.sort,
-                      color: Colors.white,
-                      size: 30.0,
-                    ),
-                    Icon(
-                      Icons.notifications,
-                      color: Colors.white,
-                      size: 30.0,
-                    ),
-                  ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Scaffold(
+            backgroundColor: const Color(0xFF000000),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  //AppBar
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Icon(
+                        Icons.sort,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      Icon(
+                        Icons.notifications,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const Expanded(
-                child: Text(
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
                   'BMI Calculator',
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 35.0,
                       fontWeight: FontWeight.w800),
                 ),
-              ),
-              const Expanded(
-                child: Text(
+                const SizedBox(
+                  height: 25.0,
+                ),
+                const Text(
                   'Gender',
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 18.0,
                       fontWeight: FontWeight.w600),
                 ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    BuildWidget(icon: FontAwesomeIcons.mars, gender: 'Male'),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    BuildWidget(icon: FontAwesomeIcons.venus, gender: 'Female')
-                  ],
+                const SizedBox(
+                  height: 5.0,
                 ),
-              ),
-              const Expanded(
-                child: Text(
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      BuildWidget(icon: FontAwesomeIcons.mars, gender: 'Male'),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      BuildWidget(
+                          icon: FontAwesomeIcons.venus, gender: 'Female')
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 25.0,
+                ),
+                const Text(
                   'Weight',
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 18.0,
                       fontWeight: FontWeight.w600),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.orange,
+                const SizedBox(
+                  height: 5.0,
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.purple,
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.black,
+                                    shape: const CircleBorder()),
+                                onPressed: () {
+                                  setState(() {
+                                    weight--;
+                                  });
+                                },
+                                child: const Icon(
+                                  Icons.remove,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                weight.toString(),
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.black,
+                                    shape: const CircleBorder()),
+                                onPressed: () {
+                                  setState(() {
+                                    weight++;
+                                  });
+                                },
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 35.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        child: Expanded(
+                          child: DropdownButton<String>(
+                            value: weightDropdownValue,
+                            icon: const Icon(
+                              Icons.expand_more,
+                            ),
+                            underline: Container(),
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                weightDropdownValue = newValue!;
+                              });
+                            },
+                            items: weightCategory
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.black,
+                const SizedBox(
+                  height: 25.0,
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.amber,
+                const Text(
+                  'Height',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.grey,
+                const SizedBox(
+                  height: 5.0,
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.deepOrangeAccent,
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.black,
+                                    shape: const CircleBorder()),
+                                onPressed: () {
+                                  setState(() {
+                                    height--;
+                                  });
+                                },
+                                child: const Icon(
+                                  Icons.remove,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                height.toString(),
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.black,
+                                    shape: const CircleBorder()),
+                                onPressed: () {
+                                  setState(() {
+                                    height++;
+                                  });
+                                },
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 35.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        child: Expanded(
+                          child: DropdownButton<String>(
+                            value: heightDropdownValue,
+                            icon: const Icon(
+                              Icons.expand_more,
+                            ),
+                            underline: Container(),
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                heightDropdownValue = newValue!;
+                              });
+                            },
+                            items: heightCategory
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 25.0,
+                ),
+                const Text(
+                  'Age',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.black,
+                              shape: const CircleBorder()),
+                          onPressed: () {
+                            setState(() {
+                              age--;
+                            });
+                          },
+                          child: const Icon(
+                            Icons.remove,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          age.toString(),
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.black,
+                              shape: const CircleBorder()),
+                          onPressed: () {
+                            setState(() {
+                              age++;
+                            });
+                          },
+                          child: const Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25.0,
+                ),
+                Expanded(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                          primary: Colors.white,
+                          backgroundColor: Colors.green,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0))),
+                      onPressed: () {},
+                      child: const Text(
+                        'Calculate',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
